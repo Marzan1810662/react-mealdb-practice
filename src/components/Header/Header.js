505e17634bd1109a3c41e763dc5ff146bd39e5e3
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-12 14:48:08 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import { faBowlFood } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/home">
+                        <h1><FontAwesomeIcon icon={faBowlFood} id='icon'></FontAwesomeIcon>Meal Stories</h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={NavLink} to="/learnmore">Learn More</Nav.Link>
+                            <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={NavLink} to="/aboutus">About Us</Nav.Link>
+                            <Nav.Link as={NavLink} to="/login">ogin</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
